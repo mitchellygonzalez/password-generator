c1a221f667fc37d4a2ea950b19961e4b69dbbd69
mitchellygonzalez <107009737+mitchellygonzalez@users.noreply.github.com>
2022-07-01 01:43:41 -0400

    all done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,67 @@
 // Assignment code here
-function generatePassword (){
-numbers="0123456789"
-lowercase="abcdefghijklmnopqrstuvwxyz"
-uppercase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-characters="!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+function generatePassword() {
+  const NUMBERS = "0123456789"
+  const LOWERCASE = "abcdefghijklmnopqrstuvwxyz"
+  const UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const CHARACTERS = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
 
-incomplete = true
-while (incomplete) {
-  var promptLength = window.prompt('Choose a password length between 8 and 128 characters.');
- 
-  if (parseInt(promptLength)< 8 || parseInt(promptLength)>128) {
-    window.alert("You must pick a length that is between 8 and 128 characters");
-    continue
-  } 
-  window.alert("Please pick at least 1 of the following 4 options.")
-  var promptLowercase = window.confirm('Do you want to include lowercase letters? If so, click OK.');
+  let ready = false
 
-  var promptUppercase = window.confirm('Do you want to include uppercase letters? If so, click OK.');
-  
-  var promptCharacters = window.confirm('Do you want to include special characters? If so, click OK.');
-  
-  var promptNumber = window.confirm('Do you want to include numeric characters? If so, click OK.');
+  while (!ready) {
+    var promptLength = window.prompt('Choose a password length between 8 and 128 characters.');
+    var passwordLength = parseInt(promptLength)
+    if (passwordLength < 8 || passwordLength > 128) {
+      window.alert("You must pick a length that is between 8 and 128 characters");
+      continue
+    }
+    window.alert("Please pick at least 1 of the following 4 options.")
+    var promptLowercase = window.confirm('Do you want to include lowercase letters? If so, click OK.');
 
-  prefs=[parseInt(promptLength), promptLowercase, promptUppercase, promptCharacters, promptNumber]
+    var promptUppercase = window.confirm('Do you want to include uppercase letters? If so, click OK.');
 
-  if (prefs[1]==false && prefs[2]==false && prefs[3]==false && prefs[4]==false) {
-    window.alert("You must select at least 1 of the 4 options!")
+    var promptCharacters = window.confirm('Do you want to include special characters? If so, click OK.');
+
+    var promptNumber = window.confirm('Do you want to include numeric characters? If so, click OK.');
+
+    var prefs = [parseInt(promptLength), promptLowercase, promptUppercase, promptCharacters, promptNumber]
+
+    if (prefs[1] == false && prefs[2] == false && prefs[3] == false && prefs[4] == false) {
+      window.alert("You must select at least 1 of the 4 options!")
+    }
+    else {
+      ready = true
+    }
+
+    window.alert("Validating your password...");
+    if (ready) {
+      window.alert("Your password is being generated...");
+    } else {
+      window.alert("Oh no, critical error! MELTDOWN MELTDOWN!!!");
+    }
+
   }
-  else {
-    incomplete = false
+
+  // THIS IS ALL FINE ABOVE:
+
+
+  var password = ""
+  let possibleCharacters = [LOWERCASE, UPPERCASE, CHARACTERS, NUMBERS]
+
+  let allowedCharacters =[]
+
+for (j=1;j<prefs.length; j++) {
+  if (prefs[j] == true) {
+    allowedCharacters.push(possibleCharacters[j-1])
   }
 }
 
-// THIS IS ALL FINE ABOVE:
 
-
-var password = ""
-
-allowedCharacters=[numbers, lowercase, uppercase, characters]
-
-// for (i=0;i<prefs.length;i++) {
-//   if (prefs[i]==true) {
-//     allowedCharacters.push(prefs[i])
-//   }
-// }
-
-for (i=0; i<parseInt(promptLength);i++) {
-  let type = allowedCharacters[Math.floor(Math.random()*allowedCharacters.length)]
-  let char = type[Math.floor(Math.random()*type.length)]
-  password += char
-}
-return password
+  for (i = 0; i < parseInt(promptLength); i++) {
+    let type = allowedCharacters[Math.floor(Math.random() * allowedCharacters.length)]
+    let char = type[Math.floor(Math.random() * type.length)]
+    password += char
+  }
+  return password
 }
 
 // Get references to the #generate element
